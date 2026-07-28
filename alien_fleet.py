ef00c28e701c65bbab553a1f5faf40dc36c3fb80
jakerothacker <jakerothacker@gmail.com>
2026-07-28 17:20:48 -0400

    M2 aliens now spawn on right side of screen

--- alien_fleet.py
+++ alien_fleet.py
@@ -18,28 +18,43 @@ class AlienFleet:
         self.create_fleet()
 
     def create_fleet(self):
-        alien_h = self.settings.alien_h
+        alien_w = self.settings.alien_h #h and w are switched due to rotation
+        alien_h = self.settings.alien_w #since alien is currently square not a big deal
+        screen_w = self.settings.screen_w
         screen_h = self.settings.screen_h
 
-        fleet_h = self.calculate_fleet_size(alien_h, screen_h)
+        fleet_h,fleet_w = self.calculate_fleet_size(alien_h, screen_h , alien_w, screen_w)
 
-        #half_screen = screen_w
+        half_screen = screen_w // 2
         fleet_vertical_space = fleet_h * alien_h
+        fleet_horizontal_space = fleet_w * alien_w
         y_offset = int((screen_h-fleet_vertical_space)//2)
+        x_offset = int((half_screen-fleet_horizontal_space)//2)
+       
+        for col in range(fleet_w):
+            for row in range(fleet_h):
+                current_y = alien_h *row + y_offset
+                current_x = alien_w *col + x_offset + half_screen
+                if row % 2 ==0 or col%2 == 0:
+                    continue
+                self._create_alien(current_x , current_y)
 
-        for row in range(fleet_h):
-            current_y = alien_h *row + y_offset
-            self._create_alien(1100 , current_y)
-
-    def calculate_fleet_size(self, alien_h, screen_h):
+    def calculate_fleet_size(self, alien_h, screen_h, alien_w, screen_w):
 
         fleet_h = (screen_h//alien_h)
+        fleet_w = ((screen_w//2)//alien_w)
 
         if fleet_h %2 == 0:
             fleet_h -= 1
         else:
             fleet_h -=2
-        return fleet_h
+
+        if fleet_w %2==0:
+            fleet_w -= 1
+        else:
+            fleet_w -=2
+
+        return fleet_h,fleet_w
 
 
     def _create_alien(self, current_x:int , current_y:int):
