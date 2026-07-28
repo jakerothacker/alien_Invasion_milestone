0ed1da8f3de88a89016e9cf394d0a7384be423af
jakerothacker <jakerothacker@gmail.com>
2026-07-28 17:06:25 -0400

    M2 a column of aliens are made as a fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,7 @@ from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
 from alien import Alien
+from alien_fleet import AlienFleet
 
 class AlienInvasion:
     """A class that contains the Alien Invasion game
@@ -33,7 +34,8 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.7)
 
         self.ship = Ship(self,Arsenal(self))
-        self.alien = Alien(self, 1000,10)
+        self.alien_fleet = AlienFleet(self)
+        self.alien_fleet.create_fleet()
 
     def run_game(self):
         """runs the Alien Invasion game (Turn on button)
@@ -41,7 +43,7 @@ class AlienInvasion:
         while self.running:
             self._check_events()
             self.ship.update()
-            self.alien.update()
+            #self.alien.update()
             self._update_screen()
 
             self.clock.tick(self.settings.FPS)
@@ -52,7 +54,7 @@ class AlienInvasion:
         """
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
-        self.alien.draw_alien()
+        self.alien_fleet.draw()
         pygame.display.flip()
 
     def _check_events(self):
