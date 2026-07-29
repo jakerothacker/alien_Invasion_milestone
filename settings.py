1273ee9f17c1e8c6f12ee588b4f44cba12b93f91
jakerothacker <jakerothacker@gmail.com>
2026-07-29 08:40:58 -0400

    M2 game ends after getting losing three times. Made game_stats to store changing vars

--- settings.py
+++ settings.py
@@ -19,6 +19,7 @@ class Settings:
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 5
+        self.starting_ship_count = 2
 
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
