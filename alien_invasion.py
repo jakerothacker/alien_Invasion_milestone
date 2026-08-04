c69d62c0048ae62a0ba262d8ee9b7d255cfc7bf1
jakerothacker <jakerothacker@gmail.com>
2026-08-04 07:32:39 -0400

    M3 button is up at start of game and when you run out of lives. Starts game when pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,7 @@ from alien import Alien
 from alien_fleet import AlienFleet
 from game_stats import GameStats
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     """A class that contains the Alien Invasion game
@@ -42,7 +43,8 @@ class AlienInvasion:
         self.ship = Ship(self,Arsenal(self))
         self.alien_fleet = AlienFleet(self, Arsenal(self))
         self.alien_fleet.create_fleet()
-        self.game_active = True
+        self.play_button = Button(self,"Play")
+        self.game_active = False
 
     def run_game(self):
         """runs the Alien Invasion game (Turn on button)
@@ -95,12 +97,25 @@ class AlienInvasion:
         self.alien_fleet.arsenal.arsenal.empty()
         self.alien_fleet.create_fleet()
 
+    def restart_game(self):
+         #settign up dynamice settings
+         #reset game stats
+         #update Hud
+         self._reset_level()
+         self.ship._center_ship()
+         self.game_active = True
+         pygame.mouse.set_visible(False)
+
     def _update_screen(self):
         """draws the objects on the screen and flips
         """
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
         pygame.display.flip()
 
     def _check_events(self):
@@ -111,11 +126,17 @@ class AlienInvasion:
                 self.running = False
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and self.game_active == True:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
 
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+             self.restart_game()
 
     def _check_keydown_events(self, event):
         """checks if the player pressed any keys down
