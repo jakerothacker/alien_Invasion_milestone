a8f54be4ac2606f039ed54b6f0b472f04ab1b38c
jakerothacker <jakerothacker@gmail.com>
2026-08-04 08:21:44 -0400

    Used try except  handling for checking save of hi-score rather than if statements

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,7 @@ This code is a sample code provided by Professor Gabriel Walters
 7-29-2026
 """
 from typing import TYPE_CHECKING
+import json
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
@@ -18,7 +19,39 @@ class GameStats():
         self.game = game
         self.settings = game.settings
         self.max_score = 0
+        self.init_saved_scores()
         self.reset_stats() 
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file
+        try:
+            contents = self.path.read_text()
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi-score',0)
+        except (FileNotFoundError, json.decoder.JSONDecodeError) as err:
+            self.hi_score = 0
+            self.save_scores()
+            print(f"{err} when checking HighScore")
+
+
+        # if self.path.exists() and self.path.stat.__sizeof__()>20:
+        #     contents = self.path.read_text()
+        #     scores = json.loads(contents)
+        #     self.hi_score = scores.get('hi-score',0)
+        # else:
+        #     self.hi_score = 0 
+        #     self.save_scores()
+
+    def save_scores(self):
+        scores = {
+            'hi-score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found {e}')
+
 
     def reset_stats(self):
         self.score = 0
@@ -26,25 +59,26 @@ class GameStats():
         self.level = 1
 
     def update(self,collisions):
-        #update score
         self._update_score(collisions)
         self._update_max_score()
+        self._update_hi_score()
 
     def _update_max_score(self):
         if self.score>self.max_score:
             self.max_score = self.score
-        #print(f'max:{self.max_score}')
+
+    def _update_hi_score(self):
+            if self.score>self.hi_score:
+                self.hi_score = self.score
 
     def _update_score(self, collisions):
         for alien in collisions.values():
             self.score += self.settings.alien_points
-        #print(f'basic: {self.score}')
-
-        #update high
+        
 
     def update_level(self):
         self.level += 1
-        #print(self.level)
+        
 
         
         
--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ class Settings:
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
         self.difficulty_scale = 1.1
+        self.scores_file = Path.cwd() / 'Assets' / 'file' / 'scores.json'
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
@@ -55,7 +56,7 @@ class Settings:
 
         self.fleet_speed = 2
         self.fleet_drop_speed = 40   
-        self.alien.points = 50
+        self.alien_points = 50
 
     def increase_difficulty(self):
         self.ship_speed *= self.difficulty_scale
